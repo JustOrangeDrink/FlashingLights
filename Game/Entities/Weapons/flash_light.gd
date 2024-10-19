30942252132b3827b55d395f68a7e883af07fecc
JustOrangeDrink <sadcat2005@gmail.com>
2024-10-19 22:10:53 +0300

    Assets

--- Game/Entities/Weapons/flash_light.gd
+++ Game/Entities/Weapons/flash_light.gd
@@ -2,13 +2,31 @@ extends PointLight2D
 class_name FlashLight
 
 @onready var _directional_flipper = FlipSprite.new()
+@onready var is_shooting: bool = false
+@onready var timer: int = 0
 
 func _ready() -> void:
 	visible = false
+	
 
 func control_shooting(entity: CharacterBody2D, sprite: Node2D):
-	if visible:
-		entity.velocity *= 0.5
+	if is_shooting:
+		entity.velocity *= 0.4
 		_directional_flipper.flip_on_mouse(sprite)
+		visible = true
 	else:
 		_directional_flipper.flip_on_movement(sprite, entity.velocity)
+		visible = false
+
+func shoot_stable():
+	is_shooting = true
+
+func shoot_flash():
+	is_shooting = true
+	timer += 1
+	if timer < 7:
+		visible = true
+	else:
+		visible = false
+	if timer == 15:
+		timer = 0
---
+++ Game/Entities/Weapons/timer.gd
@@ -0,0 +1,13 @@
+extends Timer
+class_name MyTimer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Replace with function body.
+	wait_time = 0.5
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
