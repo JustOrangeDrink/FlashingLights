677dc08d3a6c14b04319bcf25416c018a74a2da3
JustOrangeDrink <sadcat2005@gmail.com>
2024-10-20 14:42:06 +0300

    Tweaking flashlight around, minor architecture changes and most importantly brand-new arguably bad shaders. Also remade maptileset from scratch to fix cursed 63 errors on start-up

--- Game/Entities/Weapons/flash_light.gd
+++ Game/Entities/Weapons/flash_light.gd
@@ -2,34 +2,52 @@ extends PointLight2D
 class_name FlashLight
 
 @onready var _directional_flipper = FlipSprite.new()
-@onready var is_shooting: bool = false
+@onready var is_shooting: bool
+@onready var is_scanning: bool
 @onready var timer: int = 0
 
 func _ready() -> void:
 	visible = false
-	
 
 func control_shooting(entity: CharacterBody2D, sprite: Node2D):
 	if is_shooting:
-		entity.velocity *= 0.4
 		_directional_flipper.flip_on_mouse(sprite)
-		visible = true
+		if is_scanning:
+			entity.velocity *= 0.5
 	else:
 		_directional_flipper.flip_on_movement(sprite, entity.velocity)
 		visible = false
 
+func play_switch_sound(switch_sound: AudioStreamPlayer2D):
+	if Input.is_action_just_pressed("Left Mouse Button") or Input.is_action_just_pressed("Right Mouse Button"):
+		switch_sound.pitch_scale = 1.2
+		switch_sound.play()
+	if Input.is_action_just_released("Left Mouse Button") or Input.is_action_just_released("Right Mouse Button"):
+		switch_sound.pitch_scale = 1
+		switch_sound.play()
+
 func shoot_stable():
 	is_shooting = true
+	scale = Vector2(0.7, 0.7)
+	visible = true
+	color = "FFFFFF"
+	energy = 4
+	look_at(get_global_mouse_position())
 
-func shoot_flash():
+func scan():
 	is_shooting = true
-	var red
-	var blue
-	#TODO: Add change of color after each flash
+	is_scanning = true
+	scale.y = 1.5
+	visible = true
+	color = "00ad02"
+	energy = 3
+	
 	timer += 1
-	if timer < 7:
+	if timer == 1:
+		look_at(get_global_mouse_position())
+	if timer < 40:
 		visible = true
-	else:
+	if timer > 40:
 		visible = false
-	if timer == 15:
+	if timer == 50:
 		timer = 0
--- Game/player.gd
+++ Game/player.gd
@@ -5,44 +5,32 @@ extends CharacterBody2D
 @onready var _walk = $WalkingSound
 @onready var _flash_light = $FlashLight
 @onready var _common_animation_player = $CommonAnimationPlayer
-@onready var _flip_sprite = $FlipSprite
 
 @export var speed: int = 50
 
 func _ready() -> void:
 	_camera.limit_left = 0
-	_camera.limit_right = get_viewport().size.x - 214
+	_camera.limit_right = get_viewport().size.x - 216
 	_camera.limit_top = 0
-	_camera.limit_bottom = get_viewport().size.y - 128
+	_camera.limit_bottom = get_viewport().size.y - 129
 
 func _physics_process(_delta: float) -> void:
 	get_input()
 	_flash_light.control_shooting(self, _animated_sprite)
 	move_and_slide()
+	_flash_light.play_switch_sound($SwitchFlashLight)
 	_walk.walking_sound(self)
 	_common_animation_player.play_common_animation(self, _animated_sprite)
-	_flash_light.look_at(get_global_mouse_position())
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		_flash_light.shoot_flash()
-		_flip_sprite.flip_on_mouse(_animated_sprite)
-	elif Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
-		_flash_light.shoot_stable()
-		_flip_sprite.flip_on_mouse(_animated_sprite)
-	else:
-		_flash_light.is_shooting = false
-	
-	if Input.is_action_just_released("Left Mouse Button") or Input.is_action_just_released("Right Mouse Button"):
-		_flash_light.visible = false
 	
 func get_input() -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direction * speed
-	
-	if Input.is_action_just_pressed("Left Mouse Button"):
-		$SwitchFlashLight.pitch_scale = 1.2
-		$SwitchFlashLight.play()
-	if Input.is_action_just_released("Left Mouse Button"):
-		$SwitchFlashLight.pitch_scale = 1
-		$SwitchFlashLight.play()
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		_flash_light.scan()
+	elif Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		_flash_light.shoot_stable()
+	else:
+		_flash_light.is_shooting = false
+		_flash_light.is_scanning = false
